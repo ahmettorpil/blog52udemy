8bd8688e1236ba0b5eb65b2a96c73641d0263850
vagrant <vagrant@homestead>
2016-11-29 12:20:32 +0000

    yazar makale bolumu tamamlandı

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,11 +44,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth'       => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'admin_mi' => \App\Http\Middleware\AdminMi::class,
+        'can'        => \Illuminate\Foundation\Http\Middleware\Authorize::class,
+        'guest'      => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle'   => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin_mi'   => \App\Http\Middleware\AdminMi::class,
+        'yazar_mi'   => \App\Http\Middleware\YazarMi::class,
     ];
 }
